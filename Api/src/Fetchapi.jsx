7530b7ea32684c5563eec5d65c4034b89b0fd3f5
Anuj171018 <anujshrivastava2022@gmail.com>
2025-04-13 23:35:33 +0530

    Success

--- Api/src/Fetchapi.jsx
+++ Api/src/Fetchapi.jsx
@@ -1,23 +1,36 @@
 import axios from "axios"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 
 function Fetchapi() {
+let [apidata,setapidata] = useState([])
+
 useEffect(()=>{
     axios.get('http://localhost:3000/userdata')
-    .then(res=>console.log(res.data)
+    .then(res=>setapidata(res.data)
     )
 },[])
 
     return (
       <>
-      <h1>Data from api</h1>
-      <table>
+      <h1 align={"center"}>Data from api</h1>
+      <table border={"2"} align="center">
         <tr>
             <th>id</th>
             <th>name</th>
-            <th>n</th>
+            <th>age</th>
+            <th>contact</th>
+            <th>city</th>
         </tr>
+        {
+          apidata.map((e)=>{return <tr>
+            <td>{e.id}</td>
+            <td>{e.uname}</td>
+            <td>{e.age}</td>
+            <td>{e.contact}</td>
+            <td>{e.city}</td>
+          </tr>})
+        }
       </table>
         
       </>
--- Map/src/App.jsx
+++ Map/src/App.jsx
@@ -3,11 +3,25 @@ import myimg from './assets/react.svg'
 function App() {
  let product = [{
   id:1,pname:"mobile",pprice:20000,pimg:myimg
-  // id:2,pname:"laptop",pprice:10000,pimg:myimg
- }]
+ }
+,{
+  id:2,pname:"laptop",pprice:10000,pimg:myimg
+}]
 
   return (
     <>
+    <div style={{display:"flex"}}>
+
+    product.map((e)=>{return <div style={{border:"2px solid black"}}>
+      
+      <h1>Product name:{e.pname}</h1>
+      <h1>Product price:{e.pprice}</h1>
+      <img src="{e.pimg}" alt="" />
+      <button>Add to cart</button>
+      
+      </div>})
+
+    </div>
      
     </>
   )
